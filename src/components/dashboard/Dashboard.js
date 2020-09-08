dcd9e6a69c84bea824c063acff69efe1d251c96f
J-Waz <jgw.jakegeorge@gmail.com>
2020-09-08 09:51:46 -0500

    created store with dummy data

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import Notifications from "./Notifications";
 import NoteList from "../notes/NoteList";
+import { connect } from 'react-redux'
 
 class Dashboard extends Component {
   render() {
+    const { notes } = this.props
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <NoteList />
+            <NoteList notes={notes}/>
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
@@ -19,4 +21,10 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  return{
+    notes: state.note.notes
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard);
--- src/components/notes/NoteSummary.js
+++ src/components/notes/NoteSummary.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const NoteSummary = () => {
+const NoteSummary = ({note}) => {
   return (
     <div className="card z-depth-0 note-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">Note Title</span>
-        <p>Posted by User: J-waz</p>
+        <span className="card-title">{note.title}</span>
+        <p>{note.user_id}</p>
         <p className="grey-text">Posted @ : 12:00am</p>
       </div>
     </div>
